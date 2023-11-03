73566b7646af23ac12ab351c78fc94381345863e
Vu Nguyen <longvu2907@gmail.com>
2023-11-04 01:30:00 +0700

    improve discover courses page

--- lib/pages/discover_page.dart
+++ lib/pages/discover_page.dart
@@ -3,8 +3,28 @@ import 'package:lettutor/widgets/course_card.dart';
 import 'package:lettutor/widgets/custom_app_bar.dart';
 import 'package:lettutor/widgets/page_header.dart';
 
-class DiscoverPage extends StatelessWidget {
+class DiscoverPage extends StatefulWidget {
   const DiscoverPage({super.key});
+
+  @override
+  State<DiscoverPage> createState() => _DiscoverPageState();
+}
+
+class _DiscoverPageState extends State<DiscoverPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  late int _activeIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(() {
+      setState(() {
+        _activeIndex = _tabController.index;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +36,7 @@ class DiscoverPage extends StatelessWidget {
         child: Container(
           margin: const EdgeInsets.all(10),
           child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
+            // mainAxisSize: MainAxisSize.min,
             children: [
               // discover course header
               const PageHeader(
@@ -26,84 +45,188 @@ class DiscoverPage extends StatelessWidget {
                 subtitle:
                     'Discover the courses that are right for you\nYou can search for courses by subject, level, or teacher',
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-              // discover course list
-              // English for traveling
-              Text(
-                'English For Traveling',
-                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
+              TabBar(
+                isScrollable: true,
+                controller: _tabController,
+                onTap: (index) => setState(() {}),
+                tabs: const [
+                  Tab(
+                    text: 'Course',
+                  ),
+                  Tab(
+                    text: 'E-Book',
+                  ),
+                  Tab(
+                    text: 'Interactive E-Book',
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 350,
-                child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  shrinkWrap: true,
-                  primary: false,
-                  itemBuilder: (context, index) {
-                    return const CourseCard();
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const SizedBox(height: 20);
-                  },
-                  itemCount: 5,
-                ),
-              ),
-              const SizedBox(height: 30),
+              const SizedBox(height: 20),
 
-              // english for beginners
-              Text(
-                'English For Beginners',
-                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-              SizedBox(
-                height: 350,
-                child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  shrinkWrap: true,
-                  primary: false,
-                  itemBuilder: (context, index) {
-                    return const CourseCard();
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const SizedBox(height: 20);
-                  },
-                  itemCount: 5,
-                ),
-              ),
-              const SizedBox(height: 30),
-
-              // Business english
-              Text(
-                'Business English',
-                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-              SizedBox(
-                height: 350,
-                child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  shrinkWrap: true,
-                  primary: false,
-                  itemBuilder: (context, index) {
-                    return const CourseCard();
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const SizedBox(height: 20);
-                  },
-                  itemCount: 5,
-                ),
-              ),
-              const SizedBox(height: 30),
+              _buildTabView(_activeIndex),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildTabView(index) {
+    switch (index) {
+      case 0:
+        return _courseTab();
+      case 1:
+        return _bookTab();
+      case 2:
+        return _interactiveBookTab();
+      default:
+        return _courseTab();
+    }
+  }
+
+  Widget _courseTab() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // discover course list
+        // English for traveling
+        Text(
+          'English For Traveling',
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+        SizedBox(
+          height: 350,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            primary: false,
+            itemBuilder: (context, index) {
+              return const CourseCard();
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(height: 20);
+            },
+            itemCount: 5,
+          ),
+        ),
+        const SizedBox(height: 30),
+
+        // english for beginners
+        Text(
+          'English For Beginners',
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+        SizedBox(
+          height: 350,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            primary: false,
+            itemBuilder: (context, index) {
+              return const CourseCard();
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(height: 20);
+            },
+            itemCount: 5,
+          ),
+        ),
+        const SizedBox(height: 30),
+
+        // Business english
+        Text(
+          'Business English',
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+        SizedBox(
+          height: 350,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            primary: false,
+            itemBuilder: (context, index) {
+              return const CourseCard();
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(height: 20);
+            },
+            itemCount: 5,
+          ),
+        ),
+        const SizedBox(height: 30),
+      ],
+    );
+  }
+
+  Widget _bookTab() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // discover course list
+        // English for traveling
+        Text(
+          'Books',
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+        SizedBox(
+          height: 350,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            primary: false,
+            itemBuilder: (context, index) {
+              return const CourseCard();
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(height: 20);
+            },
+            itemCount: 5,
+          ),
+        ),
+        const SizedBox(height: 30),
+      ],
+    );
+  }
+
+  Widget _interactiveBookTab() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // discover course list
+        // English for traveling
+        Text(
+          'Books',
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+        SizedBox(
+          height: 350,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            primary: false,
+            itemBuilder: (context, index) {
+              return const CourseCard();
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(height: 20);
+            },
+            itemCount: 5,
+          ),
+        ),
+        const SizedBox(height: 30),
+      ],
     );
   }
 }
